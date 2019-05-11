5b427c492ad1c5d4c9131f65090f59ada58458c5
radioegor146 <radioegor146@yandex.ru>
2019-05-11 21:58:05 +0300

    more fixes + flexibility

--- PassGen/MainWindow.xaml.cs
+++ PassGen/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Windows;
 
@@ -8,11 +10,13 @@ namespace PassGen
     {
         private static readonly RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
 
-        private static int GetNextNumber(int maxValue)
+        private static char GetNewChar()
         {
             var symbol = new byte[1];
             randomNumberGenerator.GetBytes(symbol);
-            return symbol[0] % maxValue;
+
+            // for more info check ASCII table
+            return (char) (symbol[0] % ('z' - '!' + 1) + '!');
         }
 
         public MainWindow()
@@ -20,50 +24,32 @@ namespace PassGen
             InitializeComponent();
         }
         
+        private delegate bool CharCheckDelegate(char value);
+        
         private void GenerateButtonClick(object sender, RoutedEventArgs e)
         {
-            // for more info check ASCII table
-
             var resultPassword = string.Empty;
 
-            const int startShift = ' ' + 1;
-            const int charRange = 'z' - '!' + 1;
+            var invalidChecks = new List<CharCheckDelegate>();
+
+            if (UseDigitsCheckBox.IsChecked == false)
+                invalidChecks.Add(char.IsDigit);
+            if (UseSpecialSymbolsCheckBox.IsChecked == false)
+                invalidChecks.Add(x => !char.IsLetterOrDigit(x));
 
             for (var i = 0; i < PasswordLengthSlider.Value; i++)
             {
-                var charValue = startShift + GetNextNumber(charRange);
-
-                if (UseDigitsCheckBox.IsChecked == true)
-                {
-                    if (UseSpecialSymbolsCheckBox.IsChecked == false)
-                    {
-                        while (charValue < '0' || charValue > '9' && charValue < 'a' || charValue > 'z' && charValue < 'A' || charValue > 'Z')
-                            charValue = startShift + GetNextNumber(charRange);
-                    }
-                }
-                else
-                {
-                    if (UseSpecialSymbolsCheckBox.IsChecked == false)
-                    {
-                        while (charValue < 'a' || charValue > 'z' && charValue < 'A' || charValue > 'Z')
-                            charValue = startShift + GetNextNumber(charRange);
-                    }
-                    else
-                    {
-                        while (charValue > '0' && charValue < '9')
-                            charValue = startShift + GetNextNumber(charRange);
-                    }
-                }
-
-                resultPassword += Convert.ToChar(charValue);
+                var charValue = GetNewChar();
+                while (invalidChecks.Any(x => x(charValue)))
+                    charValue = GetNewChar();
+                resultPassword += charValue;
             }
             ResultTextBox.Text = resultPassword;
         }
 
         private void PasswordLengthSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            if (CurrentPasswordLengthLabel != null)
-                CurrentPasswordLengthLabel.Content = PasswordLengthSlider.Value;
+            CurrentPasswordLengthLabel.Content = PasswordLengthSlider.Value;
         }
 
         private void CopyButtonClick(object sender, RoutedEventArgs e)
