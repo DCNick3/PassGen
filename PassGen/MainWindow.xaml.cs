b1c04cb2b45648ee1a86da3363a3e274bb7a5d51
Paul <paul.kalug@yandex.ru>
2019-05-11 20:19:43 +0300

    Cryptosafe random + increased maximum password length

--- PassGen/MainWindow.xaml.cs
+++ PassGen/MainWindow.xaml.cs
@@ -1,48 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PassGen
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        Random rnd = new Random();
-        int c = 0;
+        private RandomNumberGenerator rnd = RandomNumberGenerator.Create();
         public MainWindow()
         {
             InitializeComponent();
             PLength.Minimum = 1;
-            PLength.Maximum = 25;
-            PLength.Value = 1;
+            PLength.Maximum = 40;
+            PLength.Value = 10;
         }
 
+        private int getNewNumber(int mx)
+        {
+            byte[] symbol = new byte[1];
+            rnd.GetBytes(symbol);
+            return symbol[0] % mx;
+        }
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string s = String.Empty;
             for (int i = 0; i < PLength.Value; i++)
             {
-                int xchar = 33 + rnd.Next(94);
+                int xchar = 33 + getNewNumber(94);
                 if (Digits.IsChecked == true)
                 {
                     if (Symbols.IsChecked == false)
                     {
                         while (xchar < 48 || xchar > 57 && xchar < 65 || xchar > 90 && xchar < 97 || xchar > 122)
                         {
-                            xchar = 33 + rnd.Next(94);
+                            xchar = 33 + getNewNumber(94);
                         }
                     }
                 }
@@ -52,14 +45,14 @@ namespace PassGen
                     {
                         while (xchar < 65 || xchar > 90 && xchar < 97 || xchar > 122)
                         {
-                            xchar = 33 + rnd.Next(94);
+                            xchar = 33 + getNewNumber(94);
                         }
                     }
                     else
                     {
                         while (xchar > 47 && xchar < 58)
                         {
-                            xchar = 33 + rnd.Next(94);
+                            xchar = 33 + getNewNumber(94);
                         }
                     }
                 }
